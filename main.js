d375655a3762101674af2b8d8b94f79084c5b13e
Son Nutchapon <sonnutchapon@gmail.com>
2021-01-05 16:50:53 +0700

    create rooms

--- main.js
+++ main.js
@@ -10,12 +10,15 @@ class Command {
 function main() {
   const filename = 'input.txt'
   const commands = getCommandsFromFileName(filename)
+  let rooms = []
 
   commands.forEach(command => {
     switch (command.name) {
       case 'create_hotel':
         const [floor, roomPerFloor] = command.params
         const hotel = { floor, roomPerFloor }
+        rooms = createRooms(hotel)
+          console.log('rooms',rooms)
 
         console.log(
           `Hotel created with ${floor} floor(s), ${roomPerFloor} room(s) per floor.`
@@ -25,6 +28,24 @@ function main() {
         return
     }
   })
+}
+
+function createRooms ({ floor, roomPerFloor }){
+  let rooms = []
+  for(let i=1;i <= floor; i++){
+    for(let j=1; j <= roomPerFloor; j++){
+      const info = {
+        no: `${i}0${j}`,
+        isBooked: false,
+        booker: {
+          name: undefined,
+          age: undefined,
+        }
+      }
+      rooms.push(info)
+    }
+  }
+  return rooms
 }
 
 function getCommandsFromFileName(fileName) {
